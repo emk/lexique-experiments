4efcf7ef4e5c5efc7acedca5188b9026a52f4115
Eric Kidd <git@randomhacks.net>
2014-09-08 16:05:44 -0400

    Add conjugators for several more major verb families

--- analyze_prototypes.py
+++ analyze_prototypes.py
@@ -9,21 +9,11 @@ import sqlite3
 import prototype
 
 # Sources:
+#   http://fr.wiktionary.org/wiki/Annexe:Conjugaison_en_fran%C3%A7ais
 #   http://www.french-linguistics.co.uk/grammar/irregular_verbs_paradigms.shtml
 #   http://french.about.com/od/grammar/a/irregular-re-verbs.htm (and related)
 
-# Don't even try to find patterns here. The first four are entirely
-# irregular, the latter three we might be able to something with at some
-# point.
-HIGHLY_IRREGULAR = ['être', 'avoir', 'aller', 'faire', 'pouvoir', 'vouloir',
-                    'savoir']
-
-# The "official" regular endings.
-GROUP_ER_PRESENT_ENDINGS = ['e', 'es', 'e', 'ons', 'ez', 'ent']
-GROUP_IR_PRESENT_ENDINGS = ['is', 'is', 'it', 'ions', 'issez', 'issent']
-GROUP_RE_PRESENT_ENDINGS = ['s', 's', '', 'ons', 'ez', 'ent']
-
-# More big verb groups:
+# More big verb families that don't quite follow the rules:
 #   dormir: Drop characteristic consonant in present singular.
 #   couvrir: Conjugate like -er verbs.
 #   prendre: Drops -d- in plural, doubles -n- in third person plural.
@@ -33,7 +23,7 @@ GROUP_RE_PRESENT_ENDINGS = ['s', 's', '', 'ons', 'ez', 'ent']
 #   craindre: Drop the -d, change to -gn- in plural.
 
 # Conjugate a group of verbs.
-class Conjugator:
+class Conjugator(object):
     def is_implemented(self):
         return False
 
@@ -41,7 +31,8 @@ class Conjugator:
         raise NotImplementedError()
 
     def present_particples(self, infinitive):
-        return [r + "ant" for r in self.atonic_radicals(infinitive)]
+        atonic_r = self.atonic_radicals(infinitive)
+        return self._simple_forms(atonic_r, ['ant'])[0]
 
     def singular_radicals(self, infinitive):
         raise NotImplementedError()
@@ -58,37 +49,95 @@ class Conjugator:
     def simple_past_radicals(self, infinitive):
         raise NotImplementedError()
 
-    # Like simple_past_radicals, but add a cicumflex to the stem.
-    #def simple_past_circumflex_radicals(self, infinitive):
-    #    past_r = self.simple_past_radicals(infinitive)
-    #    # Add a combining circumflex, then normalize to get a regular
-    #    # composed character.
-    #    return [normalize('NFC', r + u'\u0302') for r in past_r]
+    # General suffix application rules that simplify life elsewhere.
+    def _apply_suffix(self, radical, suffix):
+        if re.search('g$', radical) and not re.search(u'^[ieéè]', suffix):
+            # Preserve 'g' sounds.
+            radical = radical + u'e'
+        elif re.search('d$', radical) and suffix == u't':
+            # Assimilate trailing 't' suffixes.
+            suffix = u''
+        # Normalize Unicode to support simple_past_radicals which begin
+        # with a combining cicumflex.
+        return normalize('NFC', unicode(radical+suffix))
 
+    # Given pairs of radical lists and endings, generate all forms.
+    def _forms(self, patterns):
+        return [[self._apply_suffix(r, s) for r in radicals]
+                for (radicals,s) in patterns]
+
+    # Generate a set of forms which all use the same radical.
+    def _simple_forms(self, radicals, suffixes):
+        return self._forms([(radicals, s) for s in suffixes])
+
+    # Suffixes used by the present tense.
+    def present_suffixes(self):
+        return ['s', 's', 't', 'ons', 'ez', 'ent']
+
+    # Generate present tense forms.
     def present(self, infinitive):
-        raise NotImplementedError()
+        sing_r = self.singular_radicals(infinitive)
+        atonic_r = self.atonic_radicals(infinitive)
+        tonic_r = self.tonic_radicals(infinitive)
+        s = self.present_suffixes()
+        return self._forms([
+            (sing_r, s[0]), (sing_r, s[1]), (sing_r, s[2]),
+            (atonic_r, s[3]), (atonic_r, s[4]), (tonic_r, s[5])])
 
+    # Generate imperative forms.
     def imperative(self, infinitive):
-        raise NotImplementedError()
+        present = self.present(infinitive)
+        return [present[1], present[3], present[4]]
 
+    # Generate imperfect forms.
     def imperfect(self, infinitive):
-        raise NotImplementedError()
+        atonic_r = self.atonic_radicals(infinitive)
+        suffixes = ['ais', 'ais', 'ait', 'ions', 'iez', 'aient']
+        return self._simple_forms(atonic_r, suffixes)
 
+    # Generate subjunctive forms.
     def subjunctive(self, infinitive):
-        raise NotImplementedError()
+        tonic_r = self.tonic_radicals(infinitive)
+        atonic_r = self.atonic_radicals(infinitive)
+        return self._forms([
+            (tonic_r, 'e'), (tonic_r, 'es'), (tonic_r, 'e'),
+            (atonic_r, 'ions'), (atonic_r, 'iez'), (tonic_r, 'ent')])
 
+    # Generate future forms.
     def future(self, infinitive):
-        raise NotImplementedError()
+        future_r = self.future_radicals(infinitive)
+        suffixes = ['ai', 'as', 'a', 'ons', 'ez', 'ont']
+        return self._simple_forms(future_r, suffixes)
 
+    # Generate conditional forms.
     def conditional(self, infinitive):
-        raise NotImplementedError()
+        future_r = self.future_radicals(infinitive)
+        suffixes = ['ais', 'ais', 'ait', 'ions', 'iez', 'aient']
+        return self._simple_forms(future_r, suffixes)
 
+    # Suffixes used by the simple past tense.  These may begin with
+    # Unicode combining characters.
+    def simple_past_suffixes(self):
+        return ['s', 's', 't', u'\u0302mes', u'\u0302tes', u'rent']
+
+    # Generate the simple past tense forms.
     def simple_past(self, infinitive):
-        raise NotImplementedError()
+        past_r = self.simple_past_radicals(infinitive)
+        suffixes = self.simple_past_suffixes()
+        return self._simple_forms(past_r, suffixes)
 
+    # Suffixes used by the simple past tense.  These may begin with
+    # Unicode combining characters.
+    def subjunctive_imperfect_suffixes(self):
+        return ['sse', 'sses', u'\u0302t', 'ssions', 'ssiez', 'ssent']
+
+    # Generate the subjunctive imperfect forms.
     def subjunctive_imperfect(self, infinitive):
-        raise NotImplementedError()
+        past_r = self.simple_past_radicals(infinitive)
+        suffixes = self.subjunctive_imperfect_suffixes()
+        return self._simple_forms(past_r, suffixes)
 
+    # Called internally to compare verb forms with a Prototype object.
     def _assert_matches(self, prototype, method_name):
         expected = getattr(prototype, 'example_' + method_name)()
         got = getattr(self, method_name)(prototype.infinitive)
@@ -96,8 +145,11 @@ class Conjugator:
             err = (u"Mismatch for %s of %s (%s)\n  Expected: %s\n  Got: %s" %
                    (method_name, prototype.infinitive, prototype.label,
                     expected, got))
-            raise AssertionError(err)
+            print(err, file=sys.stderr)
+            raise AssertionError()
 
+    # Generate all verb forms and ensure they match those generated by
+    # the specified Prototype object.
     def assert_matches_prototype(self, prototype):
         self._assert_matches(prototype, 'past_participles')
         self._assert_matches(prototype, 'present_particples')
@@ -126,6 +178,12 @@ CONJUGATOR_BY_LABEL[u'être'] = IrregularConjugator()
 CONJUGATOR_BY_LABEL[u'avoir'] = IrregularConjugator()
 CONJUGATOR_BY_LABEL[u'aller'] = IrregularConjugator()
 CONJUGATOR_BY_LABEL[u'.*faire'] = IrregularConjugator()
+CONJUGATOR_BY_LABEL[u'pouvoir'] = IrregularConjugator()
+CONJUGATOR_BY_LABEL[u'.*vouloir'] = IrregularConjugator()
+CONJUGATOR_BY_LABEL[u'.*savoir'] = IrregularConjugator()
+CONJUGATOR_BY_LABEL[u'.*voir|.*oir'] = IrregularConjugator() # TODO: Replace.
+CONJUGATOR_BY_LABEL[u'.*venir'] = IrregularConjugator() # TODO: Replace.
+CONJUGATOR_BY_LABEL[u'.*devoir'] = IrregularConjugator()
 
 class ErConjugator(Conjugator):
     def is_implemented(self):
@@ -146,60 +204,112 @@ class ErConjugator(Conjugator):
     def future_radicals(self, infinitive):
         return [infinitive]
 
-    # TODO: We should consider including the 'a'.
     def simple_past_radicals(self, infinitive):
+        # Put the 'a' into our endings, because of 'èrent'.
         return [re.sub(u'er$', u'', infinitive)]
 
-    def present(self, infinitive):
-        sing_r = self.singular_radicals(infinitive)
-        atonic_r = self.atonic_radicals(infinitive)
-        tonic_r = self.tonic_radicals(infinitive)
-        return [sing_r, [r+'s' for r in sing_r], sing_r,
-                [r+'ons' for r in atonic_r],
-                [r+'ez' for r in atonic_r],
-                [r+'ent' for r in tonic_r]]
+    def present_suffixes(self):
+        return ['', 's', '', 'ons', 'ez', 'ent']
 
     def imperative(self, infinitive):
+        inherited = super(ErConjugator, self).imperative(infinitive)
         sing_r = self.singular_radicals(infinitive)
-        present = self.present(infinitive)
-        return [sing_r, present[3], present[4]]
+        return [sing_r] + inherited[1:]
 
-    def imperfect(self, infinitive):
-        atonic_r = self.atonic_radicals(infinitive)
-        suffixes = ['ais', 'ais', 'ait', 'ions', 'iez', 'aient']
-        return [[r+s for r in atonic_r] for s in suffixes]
+    def simple_past_suffixes(self):
+        return ['ai', 'as', 'a', u'âmes', u'âtes', u'èrent']
 
-    def subjunctive(self, infinitive):
-        tonic_r = self.tonic_radicals(infinitive)
-        atonic_r = self.atonic_radicals(infinitive)
-        return [[r+'e' for r in tonic_r],
-                [r+'es' for r in tonic_r],
-                [r+'e' for r in tonic_r],
-                [r+'ions' for r in atonic_r],
-                [r+'iez' for r in atonic_r],
-                [r+'ent' for r in tonic_r]]
-
-    def future(self, infinitive):
-        future_r = self.future_radicals(infinitive)
-        suffixes = ['ai', 'as', 'a', 'ons', 'ez', 'ont']
-        return [[r+s for r in future_r] for s in suffixes]
-
-    def conditional(self, infinitive):
-        future_r = self.future_radicals(infinitive)
-        suffixes = ['ais', 'ais', 'ait', 'ions', 'iez', 'aient']
-        return [[r+s for r in future_r] for s in suffixes]
-
-    def simple_past(self, infinitive):
-        past_r = self.simple_past_radicals(infinitive)
-        suffixes = ['ai', 'as', 'a', u'âmes', u'âtes', u'èrent']
-        return [[r+s for r in past_r] for s in suffixes]
-
-    def subjunctive_imperfect(self, infinitive):
-        past_r = self.simple_past_radicals(infinitive)
-        suffixes = ['asse', 'asses', u'ât', 'assions', 'assiez', 'assent']
-        return [[r+s for r in past_r] for s in suffixes]
+    def subjunctive_imperfect_suffixes(self):
+        return ['asse', 'asses', u'ât', 'assions', 'assiez', 'assent']
 
 CONJUGATOR_BY_LABEL[u'.*er'] = ErConjugator()
+CONJUGATOR_BY_LABEL[u'arriver|entrer|rentrer|rester|retomber|tomber'] = \
+  ErConjugator() # être verbs.
+CONJUGATOR_BY_LABEL[u'.*ger'] = ErConjugator()
+
+class IrConjugator(Conjugator):
+    def is_implemented(self):
+        return True
+
+    def past_participles(self, infinitive):
+        return [re.sub(u'r$', u'', infinitive)]
+
+    def singular_radicals(self, infinitive):
+        return [re.sub(u'r$', u'', infinitive)]
+
+    def atonic_radicals(self, infinitive):
+        return [re.sub(u'r$', u'ss', infinitive)]
+
+    def tonic_radicals(self, infinitive):
+        return [re.sub(u'r$', u'ss', infinitive)]
+
+    def future_radicals(self, infinitive):
+        return [infinitive]
+
+    def simple_past_radicals(self, infinitive):
+        return [re.sub(u'r$', u'', infinitive)]
+
+CONJUGATOR_BY_LABEL[u'.*ir'] = IrConjugator()
+
+class ReConjugator(Conjugator):
+    def is_implemented(self):
+        return True
+
+    def past_participles(self, infinitive):
+        return [re.sub(u're$', u'u', infinitive)]
+
+    def singular_radicals(self, infinitive):
+        return [re.sub(u're$', u'', infinitive)]
+
+    def atonic_radicals(self, infinitive):
+        return [re.sub(u're$', u'', infinitive)]
+
+    def tonic_radicals(self, infinitive):
+        return [re.sub(u're$', u'', infinitive)]
+
+    def future_radicals(self, infinitive):
+        return [re.sub(u're$', u'r', infinitive)]
+
+    def simple_past_radicals(self, infinitive):
+        return [re.sub(u're$', u'i', infinitive)]
+
+CONJUGATOR_BY_LABEL[u'.*andre|.*endre|.*ondre|.*erdre|.*ordre|.*eurdre'] = \
+  ReConjugator()
+
+class PrendreConjugator(ReConjugator):
+    def past_participles(self, infinitive):
+        return [re.sub(u'endre$', u'is', infinitive)]
+
+    def atonic_radicals(self, infinitive):
+        return [re.sub(u'endre$', u'en', infinitive)]
+
+    def tonic_radicals(self, infinitive):
+        return [re.sub(u'endre$', u'enn', infinitive)]
+
+    def simple_past_radicals(self, infinitive):
+        return [re.sub(u'endre$', u'i', infinitive)]
+    
+CONJUGATOR_BY_LABEL[u'.*prendre'] = PrendreConjugator() # TODO: Replace.
+
+class DireConjugator(ReConjugator):
+    def past_participles(self, infinitive):
+        return [re.sub(u're$', u't', infinitive)]
+
+    def atonic_radicals(self, infinitive):
+        return [re.sub(u're$', u's', infinitive)]
+
+    def tonic_radicals(self, infinitive):
+        return [re.sub(u're$', u's', infinitive)]
+
+    def simple_past_radicals(self, infinitive):
+        return [re.sub(u're$', u'', infinitive)]
+
+    def present(self, infinitive):
+        inherited = super(DireConjugator, self).present(infinitive)
+        inherited[4] = ['dites']
+        return inherited
+
+CONJUGATOR_BY_LABEL[u'.*dire'] = DireConjugator()
 
 # Open our database.
 db = sqlite3.connect("lexique.sqlite3")
