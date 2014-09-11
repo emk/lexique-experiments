2618f4ad4adee5a67ed0d10e15ed085f23e5cd9c
Eric Kidd <git@randomhacks.net>
2014-09-11 11:54:43 -0400

    Add a function for dumping data to a spreadsheet

--- lexique.py
+++ lexique.py
@@ -14,3 +14,9 @@ conn = sqlite3.connect("lexique.sqlite3")
 # Run a simple SQL command and return the result.
 def sql(command, **kw):
     return pd.read_sql(command, conn, **kw)
+
+# Save a pandas table as a TSV file.
+def save_tsv(name, data):
+    f = open(name, 'w')
+    f.write(data.to_csv(sep="\t", encoding="utf-8"))
+    f.close()
